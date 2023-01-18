be697b8dd247b73a71ae8c68c9f4988261caf0c3
Hugo Medeiros <josehugotavares7@hotmail.com>
2023-01-18 12:07:47 -0300

    primeiro commit

---
+++ crud-spring/src/main/java/com/loiane/CrudSpringApplication.java
@@ -0,0 +1,38 @@
+package com.loiane;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import com.loiane.enums.Category;
+import com.loiane.enums.Status;
+import com.loiane.model.Course;
+import com.loiane.repository.CourseRepository;
+
+@SpringBootApplication
+public class CrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringApplication.class, args);
+	}
+
+	@Bean
+	@Profile("dev")
+	CommandLineRunner initDatabase(CourseRepository courseRepository) {
+		return args -> extracted(courseRepository);
+	}
+
+	private void extracted(CourseRepository courseRepository) {
+		courseRepository.deleteAll();
+		for (int i = 1; i < 20; i++) {
+			Course c = new Course();
+			c.setName("Curso " + i);
+			c.setCategory(Category.FRONT_END);
+			c.setStatus(Status.ACTIVE);
+			courseRepository.save(c);
+		}
+	}
+
+}
